ba0a70e69fb1f7ab712d261a0e1a6aad489eb219
Victor Y. Fadeev <victor.y.fadeev@gmail.com>
2018-12-05 11:56:36 +0300

    Complete Minimization class

--- cSharp/test3/Test3/Minimization.cs
+++ cSharp/test3/Test3/Minimization.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace Test3
 {
@@ -14,54 +15,84 @@ namespace Test3
 		public static int MinimizeStates(IAutomaton automaton)
 		{
 			int[][] table = automaton.AutomatonTable;
+			int[] classes = GetClasses(automaton.FinalStates, table[0].Length);
 
-			HashSet<int>[] classes = GetClasses(automaton.FinalStates, table[0].Length);
-			HashSet<int>[] conversions = GetConversions(table);
-
-			return RecursiveSearch(classes, conversions);
+			return RecursiveSearch(table, classes, 2);
 		}
 
-		private static HashSet<int>[] GetClasses(int[] finalStates, int countStates)
+		private static int[] GetClasses(int[] finalStates, int countStates)
 		{
-			HashSet<int>[] classes = new HashSet<int>[2];
-
-			foreach (int elem in finalStates)
-			{
-				classes[0].Add(elem);
-			}
+			int[] classes = new int[countStates];
 
 			for (int i = 0; i < countStates; i++)
 			{
-				if (!classes[0].Contains(i))
-				{
-					classes[1].Add(i);
-				}
+				classes[i] = 1;
+			}
+
+			foreach (int elem in finalStates)
+			{
+				classes[elem] = 0;
 			}
 
 			return classes;
 		}
-
-		private static HashSet<int>[] GetConversions(int[][] table)
+		
+		private static int RecursiveSearch(int[][] table, int[] classes, int count)
 		{
-			HashSet<int>[] conversions = new HashSet<int>[table[0].Length];
+			Dictionary<int[], HashSet<int>> newClasses = CreateNewClasses(table, classes);
+			int newCount = newClasses.Keys.Count;
 
-			for (int i = 0; i < conversions.Length; i++)
+			if (count == newCount)
 			{
+				return count;
+			}
+
+			for (int i = 0; i < newCount; i++)
+			{
+				HashSet<int> states = newClasses[newClasses.Keys.ElementAt(i)];
+
+				for (int j = 0; j < states.Count; j++)
+				{
+					classes[states.ElementAt(j)] = i;
+				}
+			}
+
+			return RecursiveSearch(table, classes, newCount);
+		}
+
+		private static Dictionary<int[], HashSet<int>> CreateNewClasses(int[][] table, int[] classes)
+		{
+			Dictionary<int[], HashSet<int>> newClasses = new Dictionary<int[], HashSet<int>>();
+
+			for (int i = 0; i < classes.Length; i++)
+			{
+				int[] current = new int[1 + table.Length];
+				current[0] = classes[i];
+
 				for (int j = 0; j < table.Length; j++)
 				{
 					if (table[j][i] != error)
 					{
-						conversions[i].Add(table[j][i]);
+						current[j + 1] = classes[table[j][i]];
 					}
+					else
+					{
+						current[j + 1] = error;
+					}
+				}
+
+				try
+				{
+					newClasses[current].Add(i);
+				}
+				catch (KeyNotFoundException)
+				{
+					newClasses.Add(current, new HashSet<int>());
+					newClasses[current].Add(i);
 				}
 			}
 
-			return conversions;
-		}
-
-		private static int RecursiveSearch(HashSet<int>[] classes, HashSet<int>[] conversions)
-		{
-			return 0;
+			return newClasses;
 		}
 	}
 }
