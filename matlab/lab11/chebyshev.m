87ce5ab5a0d7c8f3a32b5e0e616816eb40893950
Victor Y. Fadeev <victor.y.fadeev@gmail.com>
2018-12-13 16:25:17 +0300

    Complete lab11

--- matlab/lab11/chebyshev.m
+++ matlab/lab11/chebyshev.m
@@ -1,6 +1,6 @@
-function x = chebyshev(x0, n)
+function x = chebyshev(x0, n, v)
     x(1) = x0;
     
     for i = 1 : n - 1
-        x(i + 1) = x(i) -  f(x(i)) / f_dx(x(i)) - f_dx_dx(x(i)) * power(f(x(i)), 2) / (2 * power(f_dx(x(i)), 3)); 
+        x(i + 1) = x(i) -  f(x(i)) / f_dx(x(i), v) - f_dx_dx(x(i), v) * power(f(x(i)), 2) / (2 * power(f_dx(x(i), v), 3)); 
     end
--- matlab/lab11/taylor.m
+++ matlab/lab11/taylor.m
@@ -1,10 +1,10 @@
-function x = taylor(x0, n)
+function x = taylor(x0, n, v)
     x(1) = x0;
     
     for i = 1 : n - 1
-        a = f_dx_dx(x(i)) / 2;
-        b = f_dx(x(i)) - x(i) * f_dx_dx(x(i));
-        c = f(x(i)) - x(i) * f_dx(x(i)) + power(x(i), 2) * f_dx_dx(x(i)) / 2;
+        a = f_dx_dx(x(i), v) / 2;
+        b = f_dx(x(i), v) - x(i) * f_dx_dx(x(i), v);
+        c = f(x(i)) - x(i) * f_dx(x(i), v) + power(x(i), 2) * f_dx_dx(x(i), v) / 2;
 
         x0 = (- b - sqrt(power(b, 2) - 4 * a * c)) / (2 * a);
         x1 = (- b + sqrt(power(b, 2) - 4 * a * c)) / (2 * a);
